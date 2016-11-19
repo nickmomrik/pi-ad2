e9cab517bd9e61844c1daf645a6bf1ab14a1a10b
Nick Momrik <nick@momrik.com>
2016-11-19 13:03:44 -0500

    Updated distance and RPM calculations to get closer to the AD2 display.

--- index.js
+++ index.js
@@ -23,20 +23,28 @@ setInterval( everySecond, 1000 );
 
 function calculateRPMs() {
     var len = spinTimes.length,
-        lastRPM = 0;
+        maxUse = 4,
+        start,
+        intervals;
+
+    if ( len < 2 ) {
+        return 0;
+    }
 
     /*
      Should this do some type of error correction in case a beep was missed?
      Like if the avg rpms varies too much, insert another beep in the sequence.
      */
 
-    if (len < 6) {
-        lastRPM = _.round( 60 / ( ( spinTimes[len - 1] - spinTimes[0] ) / ( len - 1 ) / 1000 ), 1 );
+    if ( len < maxUse ) {
+        start = 0;
+        intervals = len - 1;
     } else {
-        lastRPM = _.round( 60 / ( ( spinTimes[len - 1] - spinTimes[len - 6 ] ) / 5 / 1000 ), 1 );
+        start = len - maxUse;
+        intervals = maxUse - 1;
     }
 
-    return lastRPM;
+    return _.round( ( 60 / ( ( spinTimes[ len - 1 ] - spinTimes[ start ] ) / intervals / 1000 ) ), 4 );
 }
 
 function everySecond() {
@@ -47,27 +55,24 @@ function everySecond() {
         speed,
         out = '';
 
-    if ( spinTimes.length ) {
+    if ( spinTimes.length > 1 ) {
         second++;
         out = second + ' seconds: ';
 
-        /*
-         According to the AD2 manual...
-
-         "The Console pauses if the pedaling is less than 5 RPM for 3 seconds."
-
-         So should maybe reset the spins array if this happens, otherwise averages are wrong.
-          */
-
+        // From the manual - The Console pauses if the pedaling is less than 5 RPM for 3 seconds.
+        if ( Date.now() - spinTimes[ spinTimes.length - 1 ] > 2000 ) {
+            spinTimes = [];
+            return;
+        }
 
         /*
-            Watt & calorie formulas taken from https://unyieldingly.com/2013/02/airdyne-erg-trending/
+            Calorie formula from https://unyieldingly.com/2013/02/airdyne-erg-trending/
 
             But calories don't match up with the AD2.
          */
-        watts = _.round( ( 0.172 * Math.pow( cadence, 2 ) ) - ( 12.16 * cadence ) + 271.3, 3 );
-        if ( watts > 0 ) {
-            newCalories = ( ( watts * 3.9 ) / 60  ) / 60;
+        if ( cadence > 0 ) {
+            //newCalories = ( ( ( ( 0.172 * Math.pow( cadence, 2 ) ) - ( 12.16 * cadence ) + 271.3 ) * 3.9 ) / 60  ) / 60;
+            newCalories = ( ( ( ( ( 0.172 ) * Math.pow( cadence, 2 ) ) - ( 12.16 * cadence ) + 271.3 ) * 3.9 ) / 60  ) / 60;
             calories += newCalories;
 
             speed = cadence / ( 10 / 3 );
@@ -76,11 +81,11 @@ function everySecond() {
             distance += newDistance;
 
             //out = out + _.round( newCalories, 7) + ' new cals ';
-            out = out + Math.floor( calories ) + ' total cals ';
+            out = out + Math.floor( calories ) + ' cals ';
             //out = out + _.round(newDistance, 7) + ' new miles ';
-            out = out + _.round( distance, 3 ) + ' total miles ';
+            out = out + _.round( distance, 3 ) + ' miles ';
+            out = out + _.round( speed, 2 ) + ' mph ';
             out = out + cadence + ' RPMs ';
-            out = out + _.round(speed, 2) + ' mph ';
             //out = out + watts + ' watts';
             console.log(out);
         }
