83f9f08a202810430d6e9298ed10cc4568265f14
Nick Momrik <nick@momrik.com>
2016-11-20 13:58:22 -0500

    Use `express` and `socket.io` for the first version of a server/client.

--- index.js
+++ index.js
@@ -1,5 +1,30 @@
-var AD2 = require('./lib/ad2');
+var AD2 = require('./lib/ad2'),
+    express = require('express'),
+    app = express(),
+    http = require('http').Server(app),
+    io = require('socket.io')(http)
+    path = require('path');
 
-AD2.start(function(data) {
-    console.log(data);
-})
+app.use(express.static(path.join(__dirname, 'includes')));
+
+app.get('/', function(req, res){
+    res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function(socket){
+    console.log('connection');
+
+    socket.on('start', function(socket){
+        console.log('start');
+
+        AD2.start(sendData);
+    });
+});
+
+function sendData(data) {
+    io.emit('data', data);
+}
+
+http.listen(3000, function(){
+    console.log('listening on *:3000');
+});
