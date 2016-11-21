709684bd67e0ba930720184a89694644f3f83d60
Nick Momrik <nick@momrik.com>
2016-11-20 21:10:35 -0500

    Attempt to add chromium launch/exit.

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
-var AD2 = require('./lib/ad2'),
+var AD2     = require('./lib/ad2'),
     express = require('express'),
-    app = express(),
-    http = require('http').Server(app),
-    io = require('socket.io')(http)
-    path = require('path');
+    app     = express(),
+    http    = require('http').Server(app),
+    io      = require('socket.io')(http)
+    path    = require('path'),
+    os      = require('os'),
+    proc    = require('child_process');
 
 app.use(express.static(path.join(__dirname, 'includes')));
 
@@ -11,14 +13,14 @@ app.get('/', function(req, res){
     res.sendFile(__dirname + '/index.html');
 });
 
-io.on('connection', function(socket){
+io.on('connection', function(socket) {
     console.log('connection');
 
-    socket.on('start', function(){
+    socket.on('start', function() {
         console.log('start');
 
         AD2.start(sendData);
-    }).on('pause', function(){
+    }).on('pause', function() {
         console.log('pause');
 
         AD2.pause();
@@ -26,10 +28,18 @@ io.on('connection', function(socket){
         console.log('resume');
 
         AD2.resume();
-    }).on('stop', function(){
-            console.log('stop');
+    }).on('stop', function() {
+        console.log('stop');
 
-            process.exit();
+        AD2.pause();
+    }).on('exit', function() {
+        console.log('exit');
+
+        if ('Linux' == os.type()) {
+            chromium.kill('SIGINT');
+        }
+
+        process.exit();
     });
 });
 
@@ -40,3 +50,7 @@ function sendData(data) {
 http.listen(3000, function(){
     console.log('listening on *:3000');
 });
+
+if ('Linux' == os.type()){
+    var chromium = proc.spawn('chromium-browser --noerrdialogs --kiosk http://localhost:3000');
+}
