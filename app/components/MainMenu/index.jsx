3bc9e2c6ebb76fa08b23ca48954e8e6b245525a8
Nick Momrik <nick@momrik.com>
2016-11-27 22:01:09 -0500

    MainMenu: remove ThemeProvider and switch from Paper to Card.

--- app/components/MainMenu/index.jsx
+++ app/components/MainMenu/index.jsx
@@ -1,36 +1,33 @@
 import React from 'react';
-import Paper from 'material-ui/Paper';
+import {Card} from 'material-ui/Card';
 import {List, ListItem} from 'material-ui/List';
 import TimerIcon from 'material-ui/svg-icons/image/timer';
 import SettingsIcon from 'material-ui/svg-icons/action/settings';
 import ExitIcon from 'material-ui/svg-icons/action/exit-to-app';
-import ThemeProvider from 'react-theme-provider';
 
 
 export default class MainMenu extends React.Component {
     render() {
         return (
-            <ThemeProvider>
-                <Paper>
-                    <List>
-                        <ListItem
-                            primaryText="Workout Now"
-                            leftIcon={<TimerIcon />}
-                            href="/#/timer"
-                        />
-                        <ListItem
-                            primaryText="Settings"
-                            leftIcon={<SettingsIcon />}
-                            href="/#/settings"
-                        />
-                        <ListItem
-                            primaryText="Exit"
-                            leftIcon={<ExitIcon />}
-                            href="/#/exit"
-                        />
-                    </List>
-                </Paper>
-            </ThemeProvider>
+            <Card>
+                <List>
+                    <ListItem
+                        primaryText="Workout Now"
+                        leftIcon={<TimerIcon />}
+                        href="/#/timer"
+                    />
+                    <ListItem
+                        primaryText="Settings"
+                        leftIcon={<SettingsIcon />}
+                        href="/#/settings"
+                    />
+                    <ListItem
+                        primaryText="Exit"
+                        leftIcon={<ExitIcon />}
+                        href="/#/exit"
+                    />
+                </List>
+            </Card>
         );
     }
 }
