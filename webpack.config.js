b6633dc38bcabe70917fb6093373fbccdfff0290
Nick Momrik <nick@momrik.com>
2018-02-25 15:12:30 -0500

    webpack.NoErrorsPlugin was renamed to NoEmitOnErrorsPlugin

--- webpack.config.js
+++ webpack.config.js
@@ -22,7 +22,7 @@ module.exports = {
 			filename: 'index.html',
 		}),
 		new webpack.HotModuleReplacementPlugin(),
-		new webpack.NoErrorsPlugin(),
+		new webpack.NoEmitOnErrorsPlugin(),
 		new webpack.DefinePlugin({
 			'process.env.NODE_ENV': JSON.stringify('development'),
 		}),
