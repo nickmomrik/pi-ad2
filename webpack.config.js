c13900e657b4c209ffd099cc379867d78e23bd83
Nick Momrik <nick@momrik.com>
2018-03-01 18:25:13 -0500

    webpack: need to specify "loader" for babel.

--- webpack.config.js
+++ webpack.config.js
@@ -34,7 +34,7 @@ module.exports = {
 				exclude: /node_modules/,
 				use: [
 					{
-						loader: "babel",
+						loader: "babel-loader",
 						options: {
 							presets: [
 								"react",
--- webpack.production.config.js
+++ webpack.production.config.js
@@ -35,7 +35,7 @@ module.exports = {
 				exclude: /node_modules/,
 				use: [
 					{
-						loader: "babel",
+						loader: "babel-loader",
 						options: {
 							presets: [
 								"react",
